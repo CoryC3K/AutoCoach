4273ab059552d5eba78db8b465a71147f03c8cb2
CoryC3K <coryconger@gmail.com>
2019-01-15 16:29:36 -0500

    Add files via upload
    Added ability to resize and simplified code, broke out the 'random' generating part and put it somewhere reasonable, and easy to remove once we get some real data flowing in.

--- tkinter_Bars_Concept.py
+++ tkinter_Bars_Concept.py
@@ -4,58 +4,68 @@ from tkinter import Tk, Canvas, Frame, BOTH
 import random, time
 
 class Application(Frame):
-
-    def __init__(self, master=None):
+    def __init__(self, master='none'):
         super().__init__()
         self.initUI()
         
     def initUI(self):
         self.master.title("AutoCoach")
         self.pack(fill=BOTH, expand=1)
-        #self.place(x=0, y=0, relwidth=1, relheight=1)
-        self.width = 600
-        self.height = 50
-        w = Canvas(self, width=self.width, height=self.height)
-        backg = w.create_rectangle(0, 0, self.width, 50, fill="gray")
-        redbar = w.create_rectangle(0, 0, (self.width/2), 50, fill="red")
-        greenbar = w.create_rectangle(0, 0, (self.width/4), 50, fill="green")
-        w.pack(fill=BOTH, expand=1)
         
-        self.after(10, self.update, w, redbar, greenbar, self.width)
+        c = Canvas(self, bd=2, bg="black")
+        c.pack(fill=BOTH, expand=True)
+        c.update()
         
-    def Random_Generate(self):
-        RPre = random.randint(-10,10)
-        RAct = random.randint(-10,10)
-        return (RPre,RAct)
+        window_width = c.winfo_width()
+        window_height = c.winfo_height()
+        
+        backgr = c.create_rectangle( 0, 0, (window_width*1.0), window_height, fill="gray")
+        redbar = c.create_rectangle( 0, 0, (window_width*0.8), window_height, fill="red")
+        grnbar = c.create_rectangle( 0, 0, (window_width*0.5), window_height, fill="green")
 
-    def update(self, w, redbar, greenbar, width):   # Example just adds random # from -10,10 to the bars, to make them move
-        Nums = self.Random_Generate()
-        rx0, ry0, rx1, ry1 = w.coords(redbar)   # Redbar is Predicted Grip
-        rx1 = rx1+Nums[0]                       # If statements are to keep example
-        if rx1 > width:                         # within the bounds of the bar
-            rx1 = (width*.9)
-        if rx1 < 0:
-            rx1 = width*.4
-        w.coords(redbar, rx0,ry0,rx1,ry1)
+        self.bars(c, backgr, redbar, grnbar)
         
-        gx0, gy0, gx1, gy1 = w.coords(greenbar) # Greenbar is actual Grip
-        gx1 = gx1+Nums[1]
-        if gx1 > width:
-            gx1 = (width*.9)
-        if gx1 > rx1:
-            rx1 = gx1 + 10
-            w.coords(redbar, rx0,ry0,rx1,ry1)
-        if gx1 < 0:
-            gx1 = width*.3
-        w.coords(greenbar, gx0,gy0,gx1,gy1)
+    def bars(self, c, backgr, redbar, grnbar):
+        Nums = G_Forces()   # Read the G-forces from an outside source
+        Pred_G = Nums[0]    # It comes in as a list, so we separate them out
+        Actu_G = Nums[1]    # mostly for readable code
+        Maxx_G = Nums[2]
 
-        Tk.update(self)
-        self.after(10,self.update, w, redbar, greenbar, width)
+        width = c.winfo_width()
+        height = c.winfo_height()
+
+        c.coords(backgr, 0, 0, (Maxx_G*width), height)          # Redraw the background bar (for new width/height)
+        c.coords(redbar, 0, 0, ((Pred_G/Maxx_G)*width), height) # Redraw the Red Bar
+        c.coords(grnbar, 0, 0, ((Actu_G/Maxx_G)*width), height) # Redraw the Green Bar
+        # print("G's: ", self.lat,self.long)
+        Tk.update(self)                                         # Update app window
+        self.after(16, self.bars, c, backgr, redbar, grnbar)    # Do another cycle
+
+def G_Forces():
+    global Predicted_G, Actual_G, Max_G
+
+    ### this is all for generating random G forces
+    ### In the real code, this would pull from the API and do maths
+    RPre = random.randint(-5,5)/100
+    RAct = random.randint(-3,5)/100
+    Predicted_G = round((Predicted_G + RPre),2)
+    Actual_G    = round((Actual_G + RAct),2)
+    if Predicted_G > Max_G:
+        Max_G = Predicted_G
+    if Actual_G > Predicted_G:
+        Actual_G = Predicted_G * .8
+    #print("PredictedG: ", Predicted_G)
+    #print("ActualG:    ", Actual_G)
+    ### Okay, back to real code stuff
         
-def main():
-    root = Tk()
-    ex = Application(root)
-    root.mainloop()  
+    return (Predicted_G, Actual_G, Max_G)
 
+    
 if __name__ == '__main__':
-    main()
+    Max_G =         1.0        # Declare some random starter values
+    Predicted_G =   0.5
+    Actual_G =      0.8
+    root = Tk()
+    root.geometry("300x50")
+    e = Application(root)
+    root.mainloop()
